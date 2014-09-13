aca4380624a700a9f4e50cb785245d664150daba
babykang <wangkang@lovewkdeMacBook-Pro.local>
2014-09-13 20:20:46 +0800

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by babykang on 14-9-13.
+//  Copyright (c) 2014年 Fiona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
